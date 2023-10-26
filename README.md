3fe29255e47f215ac5c74a069f27ff0e32e5af62
Skandesh <42321593+Skandesh@users.noreply.github.com>
2023-10-27 00:52:15 +0530

    documents for the package

---
+++ README.md
@@ -0,0 +1,7 @@
+> Get to know Skandesh using your command link with Node.js by running the command
+
+## Usage
+
+To use this cli run the below command
+
+     npx skandesh
---
+++ index.js
@@ -0,0 +1,8 @@
+const pkgJSON = require("./package.json");
+console.clear();
+console.log(`NAME - ${pkgJSON.name}
+DESCRIPTION - ${pkgJSON.description}
+VERSION - ${pkgJSON.version}
+
+`);
+console.log(`Skandesh`);
